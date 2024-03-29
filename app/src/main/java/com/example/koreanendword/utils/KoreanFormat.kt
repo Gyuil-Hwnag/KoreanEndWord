a1d4a5ba2e4febe794fb577b71f42139ac9beeab
duru.hi <hwanggyuil@gmail.com>
2024-03-29 23:03:26 +0900

    feat. 네임 규칙, 예시 추가

---
+++ app/src/main/java/com/example/koreanendword/utils/KoreanFormat.kt
@@ -0,0 +1,6 @@
+package com.example.koreanendword.utils
+
+val fromFormat: List<String> = listOf("은", "는")
+val withFormat: List<String> = listOf("와", "과")
+val whoFormat: List<String> = listOf("이", "가")
+val whatFormat: List<String> = listOf("을", "를")
---
+++ app/src/main/java/com/example/koreanendword/utils/NameFormat.kt
@@ -0,0 +1,3 @@
+package com.example.koreanendword.utils
+
+val checkNames: List<String> = listOf("두루", "duru", "황규일", "duru.hi", "duru1")
