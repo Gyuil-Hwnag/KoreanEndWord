74b01f4e7654a6ebf18372bbb0bdf28be002962a
duru.hi <hwanggyuil@gmail.com>
2024-03-30 00:02:40 +0900

    fix. "이", "가" 로직 수정

--- app/src/main/java/com/example/koreanendword/utils/library/WhoFormatter.kt
+++ app/src/main/java/com/example/koreanendword/utils/library/WhoFormatter.kt
@@ -8,13 +8,14 @@ object WhoFormatter {
     fun format(name: String): String {
         val lastName = name[name.length - 1]
 
-        if (checkKorean(lastName) || checkEnglish(lastName)) {
+        if (checkKorean(lastName)) {
             if (lastName.code < 0xAC00 || lastName.code > 0xD7A3) {
                 return name + "이"
             }
             val formatValue = if ((lastName.code - 0xAC00) % 28 > 0) "이" else "가"
             return name + formatValue
         }
+        if (checkEnglish(lastName)) return name + "가"
         if (checkNumber(lastName)) return name + "이"
         return name
     }
